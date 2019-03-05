19210793fa6d73cef9254c1e9ef9b93fc92b5dce
Wojciech <wlodarczyk.woj@gmail.com>
2019-03-05 22:42:55 +0100

    Added data dump

---
+++ modules/packages/api/views/dump.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_pandas import PandasSimpleView
+
+from modules.packages.api.views.utils import get_data_dump
+from tasks.models import Mission
+
+import pandas as pd
+
+
+class MissionDumpView(PandasSimpleView):
+    def get_data(self, request, mission_id, *args, **kwargs):
+        mission = Mission.objects.filter(id=mission_id).first()
+        if mission:
+            data = get_data_dump(mission)
+            return pd.DataFrame(data)
+        raise NotFound("No Task found for given id.")
---
+++ modules/packages/api/views/utils.py
@@ -0,0 +1,15 @@
+from modules.aggregation.models import ItemAggregation
+
+
+def get_data_dump(mission):
+    data = []
+    for package in mission.packages.packages.all():
+        row = {}
+        for item in package.items.all():
+            aggregation = ItemAggregation.objects.filter(item=item).first()
+            if aggregation and aggregation.data:
+                for key, value in aggregation.data.items():
+                    row["{}_{}".format(item.order, key)] = value
+        if row:
+            data.append(row)
+    return data
