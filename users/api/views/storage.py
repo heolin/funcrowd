0e50af1f66754c0e1956b1ba7d219a3b6b9a6414
Wojciech <wlodarczyk.woj@gmail.com>
2019-04-18 23:34:26 +0200

    Added support for sentry

--- users/api/views/storage.py
+++ users/api/views/storage.py
@@ -6,38 +6,41 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from users.api.serializers.storage import (
-    StorageGetSerializer, StoragePostSerializer,
-    StorageBatchPostSerializer)
+    StorageSerializer, StoragePostSerializer
+)
 from users.models.storage import Storage
 
 
 class EndWorkerStorageView(GenericAPIView):
+    serializer_class = StoragePostSerializer
 
     def get(self, request, key, *args, **kwargs):
         storage = request.user.get_storage(key)
-        serializer = StorageGetSerializer(storage)
+        serializer = StorageSerializer(storage)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, key, *args, **kwargs):
-        serializer = StoragePostSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             storage = request.user.get_storage(key)
             storage.data = serializer.data['data']
+            print(storage.__dict__)
             storage.save()
-            serializer = StorageGetSerializer(storage)
+            serializer = StorageSerializer(storage)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EndWorkerStorageBatchView(GenericAPIView):
+    serializer_class = StorageSerializer
 
     def get(self, request, *args, **kwargs):
         storages = Storage.objects.filter(user=request.user)
-        serializer = StorageGetSerializer(storages, many=True)
+        serializer = self.serializer_class(storages, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        serializer = StorageBatchPostSerializer(data=request.data, many=True)
+        serializer = self.serializer_class(data=request.data, many=True)
         if serializer.is_valid():
             for value in serializer.data:
                 storage = request.user.get_storage(value['key'])
@@ -45,6 +48,6 @@ class EndWorkerStorageBatchView(GenericAPIView):
                 storage.save()
 
             storages = Storage.objects.filter(user=request.user)
-            serializer = StorageGetSerializer(storages, many=True)
+            serializer = self.serializer_class(storages, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
