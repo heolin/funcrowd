bac15b4f7aa7c6f451819234a72058674490ff10
Wojciech <wlodarczyk.woj@gmail.com>
2019-08-13 00:09:07 +0200

    Added default value field

--- tasks/models/item_template.py
+++ tasks/models/item_template.py
@@ -12,14 +12,19 @@ from sortedm2m.fields import SortedManyToManyField
 class ItemTemplateField(models.Model):
     name = models.CharField(max_length=30)
     label = models.CharField(max_length=100, default="", blank=True)
+    type = models.CharField(max_length=10, choices=TYPES, default=STR)
     widget = models.CharField(max_length=30)
+    comment = models.CharField(max_length=30, default="", blank=True, null=True)
+
     editable = models.BooleanField(default=False)
     required = models.BooleanField(default=True)
     feedback = models.BooleanField(default=False)
-    type = models.CharField(max_length=10, choices=TYPES, default=STR)
     validate_data_source = models.BooleanField(default=True)
-    data_source = models.ForeignKey("ItemTemplateField", on_delete=models.CASCADE, null=True, blank=True)
-    comment = models.CharField(max_length=30, default="", blank=True, null=True)
+
+    data_source = models.ForeignKey("ItemTemplateField", on_delete=models.CASCADE,
+                                    null=True, blank=True, related_name="+")
+    default_value = models.ForeignKey("ItemTemplateField", on_delete=models.CASCADE,
+                                      null=True, blank=True, related_name="+")
 
     def __str__(self):
         return "{}({}, {})".format(self.name, self.widget, self.comment)
