45f8b2ac8e37d1ff4aca4c2491a53b0fc6a0c1d0
Wojciech <wlodarczyk.woj@gmail.com>
2019-09-28 23:28:25 +0200

    Added basic admin site for achievements

--- modules/achievements/admin.py
+++ modules/achievements/admin.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
 from modules.achievements.models import (
     Achievement,
     ProgressAchievement,
@@ -11,28 +10,6 @@ from modules.achievements.models import (
 )
 
 
-class AchievementChildAdmin(PolymorphicChildModelAdmin):
-    """ Base admin class for all child models """
-    base_model = Achievement
-
-
-@admin.register(ProgressAchievement)
-class ModelBAdmin(AchievementChildAdmin):
-    base_model = ProgressAchievement
-
-
-@admin.register(ItemDoneAchievement)
-class ModelBAdmin(AchievementChildAdmin):
-    base_model = ItemDoneAchievement
-
-
-@admin.register(LoginCountAchievement)
-class ModelBAdmin(AchievementChildAdmin):
-    base_model = LoginCountAchievement
-
-
-@admin.register(Achievement)
-class AchievementAdmin(PolymorphicParentModelAdmin):
-    """ The parent model admin """
-    base_model = Achievement
-    child_models = (ProgressAchievement, ItemDoneAchievement, LoginCountAchievement)
+admin.site.register(ProgressAchievement)
+admin.site.register(ItemDoneAchievement)
+admin.site.register(LoginCountAchievement)
