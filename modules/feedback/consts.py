8e153506d720f643be564a1f1117e5a3b100e196
Wojciech <wlodarczyk.woj@gmail.com>
2019-11-06 00:02:40 +0100

    Added QUESTIONNAIRE type to feedback

--- modules/feedback/consts.py
+++ modules/feedback/consts.py
@@ -4,6 +4,7 @@ class FeedbackTypes:
     NONE = "NONE"
     BINARY = "BINARY"
     QUIZ = "QUIZ"
+    QUESTIONNAIRE = "QUESTIONNAIRE"
     POINTS = "POINTS"
     NER = "NER"
     CLASSIFICATION = "CLASSIFICATION"
@@ -13,6 +14,7 @@ FEEDBACK_TYPES = (
     (FeedbackTypes.NONE, "None"),
     (FeedbackTypes.BINARY, "Binary"),
     (FeedbackTypes.QUIZ, "Quiz"),
+    (FeedbackTypes.QUESTIONNAIRE, "Questionnaire"),
     (FeedbackTypes.POINTS, "Points"),
     (FeedbackTypes.NER, "NER"),
     (FeedbackTypes.CLASSIFICATION, "Classification")
