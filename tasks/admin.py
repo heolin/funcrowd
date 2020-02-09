bf9e88053f76083b06c42e6df2eeadffc9362114
Wojciech Wlodarczyk <wojciech.wlodarczyk@webinterpret.com>
2020-02-09 21:20:19 +0100

    Added UserTaskProgress and UserMissionProgress to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from tasks.models import (
-    Task, ItemTemplate, ItemTemplateField, Item, Annotation, Mission
-)
+    Task, ItemTemplate, ItemTemplateField,
+    Item, Annotation, Mission,
+    UserTaskProgress, UserMissionProgress)
+
 
 admin.site.register(Mission)
 admin.site.register(Task)
+admin.site.register(UserTaskProgress)
+admin.site.register(UserMissionProgress)
 admin.site.register(ItemTemplateField)
 admin.site.register(ItemTemplate)
 admin.site.register(Annotation)
